fa024f27cda6f8839aee2739adca01c9887b84ff
antoine <antoine.nunes35@gmail.com>
2025-04-01 21:27:38 +0200

    fix(test.py): chiantosse

--- test/test_app.py
+++ test/test_app.py
@@ -1,11 +1,1 @@
-from app.extensions import db
-from app import create_app
 
-#app = create_app()
-
-cheh = """with app.app_context():
-    try:
-        db.session.execute("SELECT 1")  # Exécute une requête simple
-        print("Connexion réussie à la base de données !")
-    except Exception as e:
-        print(f"Erreur de connexion : ")"""
